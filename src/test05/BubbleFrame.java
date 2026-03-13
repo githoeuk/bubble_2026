e47e96c4bcbbaee686e9b5338bb3ed1701e228f5
githoeuk <dnrdlwlsdl@gmail.com>
2026-03-13 12:12:08 +0900

    feat : bubble action 구현

---
+++ src/test05/BubbleFrame.java
@@ -0,0 +1,102 @@
+package test05;
+
+
+
+import javax.swing.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class BubbleFrame extends JFrame {
+
+    private JLabel backgroundMap;
+    private Player player;
+
+    public BubbleFrame() {
+        initData();
+        setInitLayout();
+        addEventLayout();
+        //충돌감지 백그라운드 서비스 시작
+        new Thread(new backgroundMapService(player)).start();
+    }
+
+    private void initData() {
+        setTitle("Bubble Bubble");
+        setSize(1000, 640);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        backgroundMap = new JLabel(new ImageIcon("img/backgroundMap.png")); // 익명클래스 응용
+        setContentPane(backgroundMap);
+
+        player = new Player(); // 포함관계
+    }
+
+    private void setInitLayout() {
+        setLayout(null);
+        setResizable(false); //  창 크기 고정
+        setLocationRelativeTo(null); // 화면 정중앙 배치 (프레임)
+
+        backgroundMap.add(player);
+
+        setVisible(true);
+    }
+
+    private void addEventLayout() {
+        this.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent e) {
+
+                switch (e.getKeyCode()) {
+                    case KeyEvent.VK_LEFT:
+                        //이동 중이 아니고 and 벽에 충돌하지 않은 상태일때만 left() 호출
+                        if(player.isLeft() == false && player.isLeftWallCrash() == false) {
+                            player.left();
+                        }
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        //이동 중이 아니고 and 벽에 충돌하지 않은 상태일때만 left() 호출
+                        if(player.isRight() == false && player.isRightWallCrash() == false){
+                            player.right();
+                        }
+                        break;
+                    case KeyEvent.VK_UP:
+                        player.up();
+                        break;
+                    //중력이 존재하기 때문에 down은 없음
+                    // spacebar를 통한 bubble 구현
+                    case KeyEvent.VK_SPACE:
+                        fireBubble();
+                        break;
+                } // end of switch
+            }
+
+            @Override
+            public void keyReleased(KeyEvent e) {
+
+                switch (e.getKeyCode()){
+                    case KeyEvent.VK_LEFT :
+                        player.setLeft(false);
+                        break;
+                    case KeyEvent.VK_RIGHT:
+                        player.setRight(false);
+                        break;
+                }
+            }
+
+        });
+    }
+    // todo 임시 버블 클래스 생성
+    private void fireBubble(){
+        Bubble bubble = new Bubble(player);
+
+        backgroundMap.add(bubble);
+        // 동적으로 컴포넌트가 그려지기 때문에 버그 발생 가능
+        backgroundMap.revalidate(); // 레이아웃 재계산
+        backgroundMap.repaint(); // 화면을다시 그려라
+    }
+
+    //main
+    public static void main(String[] args) {
+        new BubbleFrame();
+    } // end of main
+
+} // end of class
---
+++ src/test05/Player.java
@@ -0,0 +1,236 @@
+package test05;
+
+import javax.swing.*;
+
+public class Player extends JLabel implements Moveable {
+
+    // 플레이어의 현재 좌표 상태값
+    private int x;
+    private int y;
+
+    // 좌우 방향 이미지 (방향키에 따라서 이미지 전환)
+    private ImageIcon playerR;
+    private ImageIcon playerL;
+
+    // 속도 상수
+    private final int SPEED = 4;            // 좌우 이동 속도(픽셀단위)
+    private final int JUMP_SPEED = 2;       // 점프 낙하 속도
+    private final int JUMP_HEIGHT = 130;    // 점프 최대 높이
+
+    // 이동 상태 플래그
+    // ture = 해당 방향으로 이동 중 (while 루프 조건)
+    // fasle = 멈춤 (while 루프 탈출 -> Thread 종료)
+    private boolean left = false;
+    private boolean right = false;
+    private boolean up = false;
+    private boolean down = false;
+
+    // 벽 충돌 상태 플래그
+    private boolean leftWallCrash;
+    private boolean rightWallCrash;
+
+    // 플레이어의 현재 방향 (enum 타입)
+    // left() 메서드 호출 시 =  PlayerWay.LEFT변경
+    // RIGHT() 메서드 호출 시 =  PlayerWay.RIGHT변경
+    private PlayerWay playerWay = PlayerWay.RIGHT; // 게임 시작 시 오른쪽을 바람 봄
+
+    // getter
+
+    @Override
+    public int getX() {
+        return x;
+    }
+
+    @Override
+    public int getY() {
+        return y;
+    }
+
+    public boolean isLeft() {
+        return left;
+    }
+
+    public boolean isRight() {
+        return right;
+    }
+
+    public boolean isUp() {
+        return up;
+    }
+
+    public boolean isDown() {
+        return down;
+    }
+
+    public boolean isLeftWallCrash() {
+        return leftWallCrash;
+    }
+
+    public boolean isRightWallCrash() {
+        return rightWallCrash;
+    }
+
+    public PlayerWay getPlayerWay() {
+        return playerWay;
+    }
+
+    // Bubble Frame 의 key 이벤트에서 호출할 수 있도록 setter 설정
+    // setter
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
+
+    public void setRight(boolean right) {
+        this.right = right;
+    }
+
+    public void setUp(boolean up) {
+        this.up = up;
+    }
+
+    public void setDown(boolean down) {
+        this.down = down;
+    }
+
+    public void setLeftWallCrash(boolean leftWallCrash) {
+        this.leftWallCrash = leftWallCrash;
+    }
+
+    public void setRightWallCrash(boolean rightWallCrash) {
+        this.rightWallCrash = rightWallCrash;
+    }
+
+    public void setPlayerWay(PlayerWay playerWay) {
+        this.playerWay = playerWay;
+    }
+
+    public Player() {
+        initDate();
+        setInitLayout();
+    }
+
+    private void initDate() {
+        playerR = new ImageIcon("img/playerR.png");
+        playerL = new ImageIcon("img/playerL.png");
+    }
+
+    private void setInitLayout() {
+        // 캐릭터 초기 설정
+        x = 55;
+        y = 535;
+        setSize(50, 50);
+        //초기 방향
+        setIcon(playerR);
+        // 초기 위치
+        setLocation(x, y);
+    }
+
+    @Override
+    public void left() {
+        if (left) {
+            return; // 왼쪽으로 이동 중일 시 중복 thread 생성 방지
+        }
+        playerWay = PlayerWay.LEFT;
+        left = true;
+        setIcon(playerL);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // left 가 true인 동안 계속 이동 처리
+                // keyReleased에서 setLeft(False)가 되면 While문 탈출
+                while (left) {
+                    x = x - SPEED;
+                    setLocation(x, y);
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }).start(); // end of Thread
+    }
+
+    @Override
+    public void right() {
+        if (right) { // 쓰레드 중복 생성 제한
+            return;
+        }
+        playerWay = PlayerWay.RIGHT;
+        right = true;
+        setIcon(playerR);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (right) {
+                    x = x + SPEED;
+                    setLocation(x, y);
+                    try {
+                        Thread.sleep(10);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+        }).start(); // end of Thread
+    }
+
+    @Override
+    public void up() {
+
+        if (up) {
+            return;
+        }
+        up = true;
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //130 / 2 = 65반복 65픽셀만큼 상승
+                for (int i = 0; i < JUMP_HEIGHT / JUMP_SPEED; i++) {
+                    y = y - JUMP_SPEED;
+                    setLocation(x, y);
+                    try {
+                        Thread.sleep(5); // 5ms 간격(하강 보다 느리게 설정 (하강 : 3ms))
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                up = false; // 점프 최고점 도달 -> 상승 상태 해지
+                down(); // 하강 시작
+
+            }
+        }).start();
+    }
+
+    @Override
+    public void down() {
+
+        down = true;
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < JUMP_HEIGHT / JUMP_SPEED; i++) {
+                    y = y + JUMP_SPEED;
+                    setLocation(x, y);
+                    try {
+                        Thread.sleep(3); // 5ms 간격(하강 보다 느리게 설정 (하강 : 3ms))
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                down = false;
+            }
+        }).start();
+
+    }
+}
